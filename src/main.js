98147eeeea933ce67cd7b0685ec5461afa32b39f
lambortao <lambortao@hotmail.com>
2018-09-13 00:30:48 +0800

    引入iview ui

--- src/main.js
+++ src/main.js
@@ -7,9 +7,34 @@ import 'iview/dist/styles/iview.css';
 
 Vue.config.productionTip = false
 
-import { Button, Table } from 'iview';
+// iview的导入
+import { 
+  Button,
+  Table,
+  Icon,
+  Grid,
+  Menu,
+  Card,
+  Tabs,
+  Dropdown,
+  Input,
+  Radio,
+  Checkbox,
+  Switch,
+  Select } from 'iview';
 Vue.component('Button', Button);
 Vue.component('Table', Table);
+Vue.component('Icon', Icon);
+Vue.component('Grid', Grid);
+Vue.component('Menu', Menu);
+Vue.component('Card', Card);
+Vue.component('Tabs', Tabs);
+Vue.component('Dropdown', Dropdown);
+Vue.component('Radio', Radio);
+Vue.component('Checkbox', Checkbox);
+Vue.component('Input', Input);
+Vue.component('Switch', Switch);
+Vue.component('Select', Select);
 
 /* eslint-disable no-new */
 new Vue({
