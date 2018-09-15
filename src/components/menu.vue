18b5bf46fefe1ecaafc2ce4bad2806a4932181c6
lambortao <lambortao@hotmail.com>
2018-09-15 23:46:03 +0800

    菜单

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,9 +1,50 @@
 <template>
-  <div class="nav">这里是菜单</div>
+  <div class="nav">
+    <nav>
+      <router-link tag="p" :to="{path: '/home/'+item.url}" v-for="(item, $index) in navData">
+        <i :style="{backgroundImage: 'url(' + require(`../assets/${item.icon}`) + ')'}"></i>{{item.name}}
+      </router-link>
+    </nav>
+  </div>
 </template>
 <script>
 export default {
-  
+  data () {
+    return {
+      navData: {
+        'data': {
+          name: '数据',
+          icon: '@/assets/icon/6.png',
+          url: 'data'
+        },
+        'order': {
+          name: '订单',
+          icon: '@/assets/icon/1.png',
+          url: 'order'
+        },
+        'user': {
+          name: '用户',
+          icon: '@/assets/icon/5.png',
+          url: 'user'
+        },
+        'savings': {
+          name: '充值',
+          icon: '@/assets/icon/2.png',
+          url: 'savings'
+        },
+        'purchase': {
+          name: '采购',
+          icon: '@/assets/icon/3.png',
+          url: 'purchase'
+        },
+        'admin': {
+          name: '设置',
+          icon: '@/assets/icon/4.png',
+          url: 'admin'
+        }
+      }
+    }
+  }
 }
 </script>
 <style lang="scss">
