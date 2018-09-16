a912a89ca2ea65a35c78670b75adfd7f7ee603fb
zytao <lambortao@hotmail.com>
2018-09-16 17:52:34 +0800

    放弃iview

--- src/main.js
+++ src/main.js
@@ -3,42 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import 'iview/dist/styles/iview.css';
 
 Vue.config.productionTip = false
-
-// iview的导入
-import { 
-  Button,
-  Layout,
-  Table,
-  Icon,
-  Grid,
-  Page,
-  Menu,
-  Card,
-  Tabs,
-  Dropdown,
-  Input,
-  Radio,
-  Checkbox,
-  Select,
-  Sider } from 'iview';
-Vue.component('Button', Button);
-Vue.component('Layout', Layout);
-Vue.component('Table', Table);
-Vue.component('Icon', Icon);
-Vue.component('Grid', Grid);
-Vue.component('Page', Page);
-Vue.component('Menu', Menu);
-Vue.component('Card', Card);
-Vue.component('Tabs', Tabs);
-Vue.component('Dropdown', Dropdown);
-Vue.component('Radio', Radio);
-Vue.component('Checkbox', Checkbox);
-Vue.component('Input', Input);
-Vue.component('Select', Select);
-Vue.component('Sider', Sider);
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/views/order-list.1.vue
@@ -0,0 +1,117 @@
+<template>
+  <div id="orderList">
+    <header class="select-box">
+      <div class="select">
+        <Select v-model="model1" style="width:200px" placeholder="用户筛选">
+          <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+        </Select>
+        <Select v-model="model1" style="width:200px" placeholder="商品筛选">
+          <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+        </Select>
+      </div>
+      <div class="find">
+        <Input placeholder="输入用户名或商品名进行查找" style="width: 200px;" />
+      </div>      
+    </header>
+    <section>
+      <Table width="100%" border :columns="columns1" :data="data1"></Table>
+    </section>
+    <div class="pages">
+      <Page :total="100" show-total show-sizer show-elevator />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      cityList: [
+          {
+              value: 'New York',
+              label: 'New York'
+          },
+          {
+              value: 'London',
+              label: 'London'
+          },
+          {
+              value: 'Sydney',
+              label: 'Sydney'
+          },
+          {
+              value: 'Ottawa',
+              label: 'Ottawa'
+          },
+          {
+              value: 'Paris',
+              label: 'Paris'
+          },
+          {
+              value: 'Canberra',
+              label: 'Canberra'
+          }
+      ],
+      model1: '',
+      columns1: [
+          {
+              title: 'Name',
+              key: 'name'
+          },
+          {
+              title: 'Age',
+              key: 'age'
+          },
+          {
+              title: 'Address',
+              key: 'address'
+          }
+      ],
+      data1: [
+          {
+              name: 'John Brown',
+              age: 18,
+              address: 'New York No. 1 Lake Park',
+              date: '2016-10-03'
+          },
+          {
+              name: 'Jim Green',
+              age: 24,
+              address: 'London No. 1 Lake Park',
+              date: '2016-10-01'
+          },
+          {
+              name: 'Joe Black',
+              age: 30,
+              address: 'Sydney No. 1 Lake Park',
+              date: '2016-10-02'
+          },
+          {
+              name: 'Jon Snow',
+              age: 26,
+              address: 'Ottawa No. 2 Lake Park',
+              date: '2016-10-04'
+          }
+      ]
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import '@/assets/scss/methods.scss';
+
+header{
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+  padding-bottom: 20px;
+}
+section{
+  padding: 20px 0;
+  border-top: 1px solid $hr;
+  border-bottom: 1px solid $hr;
+}
+.pages{
+  padding-top: 20px;
+  width: 100%;
+}
+</style>
