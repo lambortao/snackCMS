880b3d86c62666252596c7039f32472cf71c736e
lambortao <lambortao@hotmail.com>
2018-09-16 21:40:59 +0800

    充值列表样式完成

--- src/views/savings-list.vue
+++ src/views/savings-list.vue
@@ -1,11 +1,160 @@
 <template>
-  <div id="savingsList">储蓄列表页</div>
+  <div id="savingsList">
+    <header>
+      <el-row :gutter="10">
+        <el-col :span="4">
+          <el-button type="primary" plain icon="el-icon-circle-plus-outline">充值</el-button>
+        </el-col>
+        <el-col :span="4" :offset="16">
+          <el-input prefix-icon="el-icon-search" v-model="input" placeholder="仅限用户"></el-input>
+        </el-col>
+      </el-row>
+    </header>
+    <section>
+      <el-table
+        :data="tableData3"
+        style="width: 100%"
+        border 
+        stripe>
+        <el-table-column
+          fixed
+          type="index"
+          width="50">
+        </el-table-column>
+        <el-table-column
+          prop="date"
+          label="日期">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="姓名">
+        </el-table-column>
+        <el-table-column
+          prop="province"
+          label="省份">
+        </el-table-column>
+        <el-table-column
+          prop="city"
+          label="市区">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="地址"
+          width="300">
+        </el-table-column>
+        <el-table-column
+          prop="zip"
+          label="邮编">
+        </el-table-column>
+      </el-table>
+    </section>
+    <footer>
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="currentPage4"
+        :page-sizes="[100, 200, 300, 400]"
+        :page-size="30"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="400">
+      </el-pagination>
+    </footer>
+  </div>
 </template>
 <script>
 export default {
-  
+  data() {
+    return {
+      currentPage1: 5,
+      currentPage2: 5,
+      currentPage3: 5,
+      currentPage4: 4,
+      input: '',
+      options: [{
+        value: '选项1',
+        label: '黄金糕'
+      }, {
+        value: '选项2',
+        label: '双皮奶'
+      }, {
+        value: '选项3',
+        label: '蚵仔煎'
+      }, {
+        value: '选项4',
+        label: '龙须面'
+      }, {
+        value: '选项5',
+        label: '北京烤鸭'
+      }],
+      value: '',
+      tableData3: [{
+        date: '2016-05-03',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1518 弄',
+        zip: 200333
+      }, {
+        date: '2016-05-02',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1518 弄',
+        zip: 200333
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1518 弄',
+        zip: 200333
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1518 弄',
+        zip: 200333
+      }, {
+        date: '2016-05-08',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1518 弄',
+        zip: 200333
+      }, {
+        date: '2016-05-06',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1518 弄',
+        zip: 200333
+      }, {
+        date: '2016-05-07',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1518 弄',
+        zip: 200333
+      }]
+    }
+  },
+  methods: {
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
-
+@import '@/assets/scss/methods.scss';
+section{
+  margin: 20px 0;
+  padding: 20px 0;
+  border-top: 1px solid $hr;
+  border-bottom: 1px solid $hr;
+}
 </style>
