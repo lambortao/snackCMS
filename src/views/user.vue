839b7de1d1457565c41df597894596a429df82e5
lambortao <lambortao@hotmail.com>
2018-11-01 23:25:07 +0800

    新增用户筛选

--- src/views/user.vue
+++ src/views/user.vue
@@ -9,7 +9,7 @@
     </header>
     <section>
       <el-table
-        :data="userList"
+        :data="dataList.show"
         style="width: 100%"
         border 
         stripe
@@ -80,11 +80,10 @@
       <el-pagination
         @size-change="handleSizeChange"
         @current-change="handleCurrentChange"
-        :current-page="currentPage4"
-        :page-sizes="[100, 200, 300, 400]"
-        :page-size="30"
+        :current-page="page.nowPage" 
+        :page-sizes="page.pageNumberArr"
         layout="total, sizes, prev, pager, next, jumper"
-        :total="400">
+        :total="page.total">
       </el-pagination>
     </footer>
   </div>
@@ -93,24 +92,55 @@
 export default {
   data() {
     return {
-      currentPage1: 5,
-      currentPage2: 5,
-      currentPage3: 5,
-      currentPage4: 4,
       input: '',
       value: '',
       userList: [],
-      loading: true
+      loading: true,
+      /**
+       * 翻页
+       * 参数一：初始的显示的页码
+       * 参数二：总数据量
+       * 参数三：每页显示的数据量
+       * 参数四：当前页面显示的数据量，默认是每页显示数据量的第一个
+       */
+      page: {
+        nowPage: 1,
+        total: 0,
+        pageNumberArr: [20, 40, 60, 100],
+        nowPageNumber: ''
+      },
+      /**
+       * 数据列表
+       * all - 从后台返回来的全部的数据
+       * select - 下拉列表筛选后的数据
+       * show - 当前页面显示的数据
+       * all > select > show
+       * 页面上显示的始终都是show的数据
+       * 
+       * 这里讲一下实现逻辑，默认进来下拉筛选的数据等于全部数据，当前页面显示的数据是根据默认的翻页数据处理后显示的数据
+       * 下拉筛选修改后会重置翻页数据，并重新计算数据总数
+       */
+      dataList: {
+        all: [],
+        show: []
+      }
     }
   },
   created () {
     this.getUserList();
+    this.page.nowPageNumber = this.page.pageNumberArr[0];
   },
   methods: {
     getUserList() {
       this.$port('user/getUserList').then(res => {
-        this.userList = res;
         this.loading = false;
+        // 获取到全部数据之后，默认情况下筛选的数据就是全部数据
+        this.dataList.all = res;
+        this.dataList.show = this.dataList.all;
+        // 计算数据总量
+        this.page.total = this.dataList.show.length;
+        // 初始重置翻页函数
+        this.pageFun(0, this.page.nowPageNumber);
       });
     },
     // 充值按钮
@@ -176,11 +206,35 @@ export default {
         });
       }).catch();
     },
-    handleSizeChange(val) {
-      console.log(`每页 ${val} 条`);
+    /**
+     * 分页函数
+     * 第一个参数是当前页码，第二个参数是单页显示的数量
+     */
+    pageFun(pagePosition, pageNumber) {
+      // 当前页面要显示的数据头 = 当前页码 * 单页显示的数量
+      pagePosition = pagePosition * pageNumber;
+      // 当前页面要显示的数据尾 = 当前页面要显示的数据头 + 单页显示的数量
+      pageNumber = pagePosition + pageNumber;
+      // 然后截取当前页面要显示的数据头到当前页面要显示的数据尾之间的数据
+      let lsPageData = this.dataList.all.slice(pagePosition, pageNumber);
+      // 清空显示的数据再重新赋值
+      this.dataList.show = [];
+      this.dataList.show = lsPageData;
     },
+    // 修改每页显示数量
+    handleSizeChange(val) {
+      // 循环查找一下，当前选择的数据量是变量中的第几个，然后通过变量进行赋值
+      // 数量修改完毕后，重置翻页函数
+      this.page.pageNumberArr.forEach((element, key) => {
+        if (val == element) {
+          this.page.nowPageNumber = this.page.pageNumberArr[key];
+          this.pageFun(0, this.page.nowPageNumber);
+        }
+      });      
+    },
+    // 跳转到第几页
     handleCurrentChange(val) {
-      console.log(`当前页: ${val}`);
+      this.pageFun(val - 1, this.page.nowPageNumber);
     }
   }
 }
