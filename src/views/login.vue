fa600e393dfd568200c4db3df3981467e8b91323
lambortao <lambortao@hotmail.com>
2018-11-05 23:17:07 +0800

    登录页面样式完成

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,14 +1,57 @@
 <template>
   <div id="login">
-    这是登录页
+    <section class="login-box">
+      <h3>{{ title }}</h3>
+      <div class="input-box">
+        <input type="text" name="userName" id="userName" placeholder="输入登录名">
+        <input type="password" name="password" id="password" placeholder="输入登录密码">
+        <input type="button" :value="button">
+      </div>
+    </section>
   </div>
 </template>
 <script>
 export default {
-  
+  data () {
+    return {
+      title: 'WELCOME',
+      button: '登录'
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+#login{
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+input{
+  display: block;
+  width: 260px;
+  height: 40px;
+  margin: 10px 0;
+  border: none;
+  border-radius: 5px;
+  outline: none;
+}
+input[type='text'],
+input[type='password']{
+  padding: 0 5px;
+  border: 1px solid #E4E7ED;
+}
+input[type='button']{
+  background-color: #409EFF;
+  color: #fff;
+  cursor: pointer;
+  transition: all 300ms;
 
+  &:active{
+    transform: scale(0.95);
+  }
+}
 </style>
