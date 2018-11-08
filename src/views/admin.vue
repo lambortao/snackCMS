ea1abf8d8a63a1ca23d300e77cf409b9d65c3ede
lambortao <lambortao@hotmail.com>
2018-11-08 22:53:29 +0800

    注销登陆按钮

--- src/views/admin.vue
+++ src/views/admin.vue
@@ -1,9 +1,22 @@
 <template>
-  <div id="admin">管理员页面</div>
+  <div id="admin">
+    <el-button plain type="danger" @click="outSigin()">注销登录</el-button>
+  </div>
 </template>
 <script>
 export default {
-  
+  methods: {
+    outSigin() {
+      this.$confirm('是否注销登录？', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        sessionStorage.removeItem('siginInfor');
+        this.$router.push({path:'/'});
+      }).catch(() => {});
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
